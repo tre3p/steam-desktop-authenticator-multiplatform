2830afd13f8bc3fd16b10cf585273882d53b20bf
tre3p <whereareyo@yahoo.com>
2024-05-03 22:01:21 +0300

    WIP

--- sdamp-ui/src/desktopMain/kotlin/com/tre3p/sdamp/App.kt
+++ sdamp-ui/src/desktopMain/kotlin/com/tre3p/sdamp/App.kt
@@ -1,16 +1,15 @@
 package com.tre3p.sdamp
 
-import androidx.compose.foundation.background
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tre3p.sdamp.mafile.MaFileReader
@@ -22,21 +21,74 @@ private const val MAFILES_DIR_NAME = "/.SDAMP-maFiles"
 private val MAFILES_BASE_DIR = File("$USER_HOME$MAFILES_DIR_NAME")
     .also { it.mkdir() }
 
+private val START_PADDING = 20.dp
+private val END_PADDING = 20.dp
+
 @Composable
+@Preview
 fun App() {
     val maFiles = MaFileReader.readMaFileDir(MAFILES_BASE_DIR.toPath().toAbsolutePath())
 
-    val twoFactorCodeText by remember { mutableStateOf("Test") }
+    val twoFactorCodeText by remember { mutableStateOf("A4D3C2DF") }
 
     MaterialTheme {
-        TwoFactorCodePlaceholder(twoFactorCodeText)
-        MaFilesList(maFiles, twoFactorCodeText)
+        Column {
+            TopButtons()
+            TwoFactorCodePlaceholder(twoFactorCodeText)
+        }
+    }
+}
+
+@Composable
+fun TopButtons() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = START_PADDING, end = END_PADDING, top = 5.dp)
+    ) {
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
+        ) {
+            Text("Import")
+        }
     }
 }
 
 @Composable
 fun TwoFactorCodePlaceholder(twoFactorCodeText: String) {
-
+    Row(
+        modifier = Modifier.padding(
+            start = START_PADDING,
+            end = END_PADDING,
+            top = 10.dp
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .border(BorderStroke(1.dp, Color.Black))
+                .height(60.dp)
+                .weight(40f),
+            contentAlignment = Alignment.Center
+        ) {
+            SelectionContainer {
+                Text(
+                    text = twoFactorCodeText,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(start = 20.dp)
+                .height(60.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
+        ) {
+            Text("Copy")
+        }
+    }
 }
 
 @Composable
