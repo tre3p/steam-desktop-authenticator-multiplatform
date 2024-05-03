1a321f01e71cbf2eb591b86e22765cc7ed36a9d1
tre3p <whereareyo@yahoo.com>
2024-05-03 20:35:56 +0300

    add window state

--- sdamp-ui/src/desktopMain/kotlin/com/tre3p/sdamp/main.kt
+++ sdamp-ui/src/desktopMain/kotlin/com/tre3p/sdamp/main.kt
@@ -1,12 +1,23 @@
 package com.tre3p.sdamp
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val state = rememberWindowState(
+        size = DpSize(400.dp, 600.dp),
+        position = WindowPosition(Alignment.Center)
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "SDAMP",
+        state = state,
     ) {
         App()
     }
