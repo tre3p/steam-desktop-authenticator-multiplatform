9b922e13ae022a35810c8ff2e7b7ceb924b651e5
tre3p <whereareyo@yahoo.com>
2024-05-05 19:44:01 +0300

    progressBar value changer added

--- sdamp-ui/src/desktopMain/kotlin/com/tre3p/sdamp/components/AuthCodeProgressBar.kt
+++ sdamp-ui/src/desktopMain/kotlin/com/tre3p/sdamp/components/AuthCodeProgressBar.kt
@@ -3,16 +3,28 @@ package com.tre3p.sdamp.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.tre3p.sdamp.misc.START_PADDING
+import com.tre3p.sdamp.steam.SteamTime
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun AuthCodeProgressBar() {
+    val steamChunkProgressBar = remember { mutableStateOf(1.0f) }
+    rememberCoroutineScope().launch {
+        while (true) {
+            // Since steam chunk values are in range 0..30 - dividing by 30f makes this values usable by progress bar
+            steamChunkProgressBar.value = SteamTime.getCurrentSteamChunk() / 30f
+            delay(500)
+        }
+    }
+
     LinearProgressIndicator(
-        progress = 1.0f,
+        progress = steamChunkProgressBar.value,
         color = Color.Gray,
         modifier = Modifier
             .padding(start = START_PADDING, end = 112.dp)
